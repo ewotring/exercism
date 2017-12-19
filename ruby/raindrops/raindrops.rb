0045fdadc12f4ca64ed02ec308636da7895bac11
Erik Wotring <ewotring@gmail.com>
2017-12-19 11:09:18 -0600

    Pass tests

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,5 +1,22 @@
 class Raindrops
   def self.convert(drop_count)
-
+    rain_lang = ""
+    if drop_count % 3 != 0 && drop_count % 5 != 0 && drop_count % 7 != 0
+      return drop_count.to_s
+    end
+    if drop_count % 3 == 0
+      rain_lang << "Pling"
+    end
+    if drop_count % 5 == 0
+      rain_lang <<  "Plang"
+    end
+    if drop_count % 7 == 0
+      rain_lang <<  "Plong"
+    end
+    return rain_lang
   end
+end
+
+module BookKeeping
+  VERSION = 3
 end
