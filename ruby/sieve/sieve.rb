ce07219bd25fb0cbac4af9c81fe413a1e5880303
Erik Wotring <ewotring@gmail.com>
2018-01-22 12:28:05 -0600

    Build algorithm to pass test for range to 10

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -5,8 +5,26 @@ class Sieve
     @spectrum = {}
     @new_spectrum = []
   end
+
   def primes
-    return @new_spectrum unless @input >= 2
-    @new_spectrum = @range
+    return @range unless @input >= 2
+    @range.each do |element|
+      @spectrum[element] = false
+    end
+    @spectrum.each do |key, value|
+      if value == false
+        @spectrum.each do |key_inner, value_inner|
+          if key_inner % key == 0 && key_inner > key
+            @spectrum[key_inner] = true
+          end
+        end
+      end
+    end
+    @spectrum.each do |key, value|
+      if value == false
+        @new_spectrum.push(key)
+      end
+    end
+    @new_spectrum
   end
 end
