f8e6160edc2dbbf525f8218817be3f434bddcaaf
Erik Wotring <ewotring@gmail.com>
2018-01-22 11:02:47 -0600

    Pass test up to 10

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -6,5 +6,15 @@ class Sieve
   def primes
     return @spectrum unless @range >= 2
     @spectrum = (2..@range).to_a
+    @new_spectrum = @spectrum
+    @new_spectrum.delete_if do |element|
+      element > 2 &&
+      element % 2 == 0
+    end
+    @new_spectrum.delete_if do |element|
+      element > 3 &&
+      element % 3 == 0
+    end
+    return @new_spectrum
   end
 end
