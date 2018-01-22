bccc057b06522dc7cbd4c6692bf57c42ed4a6f8a
Erik Wotring <ewotring@gmail.com>
2018-01-22 11:56:31 -0600

    Start over with new algorithm design

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,20 +1,11 @@
 class Sieve
-  def initialize(range)
-    @range = range
-    @spectrum = []
+  def initialize(input)
+    @input = input
+    @range = (2..@input).to_a
+    @spectrum = {}
+    @new_spectrum = []
   end
   def primes
-    return @spectrum unless @range >= 2
-    @spectrum = (2..@range).to_a
-    @new_spectrum = @spectrum
-    @new_spectrum.delete_if do |element|
-      element > 2 &&
-      element % 2 == 0
-    end
-    @new_spectrum.delete_if do |element|
-      element > 3 &&
-      element % 3 == 0
-    end
-    return @new_spectrum
+    return @new_spectrum unless @input >= 2
   end
 end
