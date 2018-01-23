38e1f05b599798e53ffd60a9c8b4b878be7540d2
Erik Wotring <ewotring@gmail.com>
2018-01-23 11:44:48 -0600

    Pass test for II

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -4,6 +4,14 @@ Integer.class_eval do
   # end
 
   def to_roman
-    "I"
+    input = self
+    roman_result = ""
+    while input > 0
+      if input < 5
+        roman_result << "I"
+        input -= 1
+      end
+    end
+    roman_result
   end
 end
