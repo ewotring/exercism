2a0917d4119c0ab80d2474ee98a828e84b891ffa
Erik Wotring <ewotring@gmail.com>
2018-02-04 22:02:27 -0600

    WIP nth prime exercise

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,6 +1,35 @@
 class Prime
   def self.nth(n)
-    # build array of prime numbers
-    # return entry at given index
+    # build array of prime numbers to the length of the input n
+    if n == 0
+      raise ArgumentError.new("There is no zeroth prime")
+    end
+
+    if n == 1
+      return 2
+    end
+    
+    if n == 2
+      return 3
+    end
+
+    i = 2
+    prime_list = [2]
+    
+=begin
+Take a number as input
+that number is the length of the array
+
+=end
+    while prime_list.length <= n
+      prime_list.each do |prime|
+        if i % prime == 0
+          prime_list.push(i)
+        end
+      end
+      i += 1
+    end
+    puts "prime_list is #{prime_list}"
+    return prime_list[-1]
   end
 end
