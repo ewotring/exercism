6c39d1235f90bce7ad14e2a44f3b4b48299a4cdd
Erik Wotring <ewotring@gmail.com>
2018-05-28 02:49:04 -0500

    WIP function for encoding

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -1,9 +1,20 @@
 var randomString = require("randomstring")
 
 class Cipher {
-  constructor( key = randomString.generate({ length: 100, charset: 'abcdefghijklmnopqrstuvwxyz'})) {
+  constructor(
+    key = randomString.generate({
+      length: 100,
+      charset: 'abcdefghijklmnopqrstuvwxyz'})) {
     this.key = key
   }
+encode(input) {
+  let outputArray = []
+  for (let i = 0; i < input.length; i++) {
+    outputArray.push(this.key.charAt(i))
+  }
+  let outputString = outputArray.join('')
+  return outputString
+}
 }
 
 module.exports = Cipher
