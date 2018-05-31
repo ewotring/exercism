081abd90ff6a31228e68b206917f65f0fb21904b
Erik Wotring <ewotring@gmail.com>
2018-05-30 21:39:36 -0500

    WIP improve decoding

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -10,19 +10,28 @@ class Cipher {
   }
   encode(input) {
     let encodeOutputArray = []
-    for (let i = 0; i < input.length; i++) {
-      let keyAdd = this.shiftString.indexOf(input.charAt(i))
+    for (let i = 0; i <= input.length; i++) {
+      let keyAdd = this.shiftString.indexOf((input.charAt(i)) % 26)
       encodeOutputArray.push(this.key.charAt(i + keyAdd))
     }
     let encodeOutputString = encodeOutputArray.join('')
+    console.log(`encodeOutputString is ${encodeOutputString}`)
     return encodeOutputString
   }
   decode(input) {
     let decodeOutputArray = []
+    console.log(`input is ${input}`)
     for (let i = 0; i < input.length; i++) {
+      // experiment with modulus
+      let keyAdd = 26 % this.shiftString.indexOf(input.charAt(i))
+      console.log(`input[i] is ${input.charAt(i)}`)
+      console.log(`keyAdd is ${keyAdd}`)
+      console.log(`${this.key.charAt(keyAdd)}`)
       decodeOutputArray.push('a')
+      // decodeOutputArray.push(this.key.charAt(i - keyAdd))
     }
     let decodeOutputString = decodeOutputArray.join('')
+    console.log(`decodeOutputString is ${decodeOutputString}`)
     return decodeOutputString
   }
 }
