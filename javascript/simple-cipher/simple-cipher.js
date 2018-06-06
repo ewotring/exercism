1d0ba85b0b226c667cbf2d8051367d78fdcd82e0
Erik Wotring <ewotring@gmail.com>
2018-06-06 18:26:57 -0500

    Keep on trying to get decipher to work properly

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -23,12 +23,16 @@ class Cipher {
     console.log(`input is ${input}`)
     for (let i = 0; i < input.length; i++) {
       // experiment with modulus
-      let keyAdd = 26 % this.shiftString.indexOf(input.charAt(i))
+      // let keyAdd = this.shiftString.indexOf(input.charAt(i)) % 26
+      let keyAdd = this.key.indexOf((input.charAt(i)) % 26)
       console.log(`input[i] is ${input.charAt(i)}`)
       console.log(`keyAdd is ${keyAdd}`)
-      console.log(`${this.key.charAt(keyAdd)}`)
-      decodeOutputArray.push('a')
-      // decodeOutputArray.push(this.key.charAt(i - keyAdd))
+      console.log(`${this.shiftString.charAt(keyAdd)}`)
+      console.log(`${this.shiftString.charAt(keyAdd % 26)}`)
+      // decodeOutputArray.push('a')
+      // decodeOutputArray.push(this.shiftString.charAt((0)))
+      decodeOutputArray.push(this.shiftString.charAt(( keyAdd % 26)))
+      // decodeOutputArray.push(this.key.charAt(keyAdd))
     }
     let decodeOutputString = decodeOutputArray.join('')
     console.log(`decodeOutputString is ${decodeOutputString}`)
