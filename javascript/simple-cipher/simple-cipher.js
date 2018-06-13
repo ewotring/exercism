38833e5f7ccea1527bf0c7668e8b5176453dfd3f
Erik Wotring <ewotring@gmail.com>
2018-06-12 22:01:34 -0500

    Improve encoding and WIP decoding

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -14,7 +14,7 @@ class Cipher {
     for (let i = 0; i < input.length; i++) {
       let keyAdd = this.shiftString.indexOf(input.charAt(i)) % 26
       console.log(`encode keyAdd is ${keyAdd}`)
-      encodeOutputArray.push(this.key.charAt(i + keyAdd))
+      encodeOutputArray.push(this.key.charAt((i + keyAdd) % 26))
     }
     let encodeOutputString = encodeOutputArray.join('')
     console.log(`encodeOutputString is ${encodeOutputString}`)
@@ -25,28 +25,10 @@ class Cipher {
     console.log(`input is ${input}`)
     for (let i = 0; i < input.length; i++) {
       // experiment with modulus
-      let keyAddShiftString = this.shiftString.indexOf(input.charAt(i)) % 26
-      // let keyAddKey = this.key.indexOf(input.charAt(i)) % 26
-      // let keyAdd = input.indexOf(this.shiftString.charAt(i))
+      let keyAddDecode = this.shiftString.indexOf(input.charAt(i)) % 26
       console.log(`input[i] is ${input.charAt(i)}`)
-      // console.log(`${input.charAt(i)}`)
-      console.log(`keyAddShiftString is ${keyAddShiftString}`)
-      console.log(`${this.shiftString.charAt(0)}`)
-      // console.log(`keyAddKey is ${keyAddKey}`)
-      // console.log(`keyAdd is ${keyAdd}`)
-      // console.log(`${this.shiftString.charAt(keyAdd)}`)
-      // console.log(`${this.shiftString.charAt(keyAdd % 26)}`)
-      // console.log(`${this.shiftString.charAt(26 - keyAdd)}`)
-      // console.log(`${this.shiftString.charAt(i - keyAdd)}`)
-      // console.log(`${this.key.charAt(keyAdd)}`)
-      // decodeOutputArray.push('a')
-      // decodeOutputArray.push(this.shiftString.charAt(0))
-      // decodeOutputArray.push(this.shiftString.charAt(keyAddShiftString - i))
-      decodeOutputArray.push(this.key.charAt(keyAddShiftString - i))
-      // decodeOutputArray.push(this.shiftString.charAt((0)))
-      // decodeOutputArray.push(this.key.charAt( keyAdd % 26))
-      // decodeOutputArray.push(this.shiftString.charAt( keyAdd ))
-      // decodeOutputArray.push(this.key.charAt(keyAdd))
+      console.log(`keyAddDecode is ${keyAddDecode}`)
+      decodeOutputArray.push(this.key.charAt((i + keyAddDecode) % 26))
     }
     let decodeOutputString = decodeOutputArray.join('')
     console.log(`decodeOutputString is ${decodeOutputString}`)
