743acddf76def36e0347f87ba2bf390105b218c5
Erik Wotring <ewotring@gmail.com>
2018-12-28 07:07:08 -0600

    Remove commented and unused code

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -4,35 +4,11 @@ export class Cipher {
   constructor(
     key,
     // Consider making the length random, with a length of at least 100.
-    // key = this.generateRandomKey(),
-    // key = randomString.generate({
-    //   length: 100,
-    //   charset: 'abcdefghijklmnopqrstuvwxyz',
-    // }),
   ) {
-    // this.key = key;
     this.shiftString = 'abcdefghijklmnopqrstuvwxyz';
     this.keyLength = 100;
-    this.key = typeof key !== 'undefined' ? key : this.generateRandomKey(
-      // this.shiftString,
-      // this.keyLength,
-    );
-    for (let i = 0; i < this.shiftString.length; i += 1) {
-      // console.log(`shiftString character is ${this.shiftString[i]}`);
-      // console.log(this.shiftString.charCodeAt(i));
-    }
-    // console.log(`constructor this.keyLength is ${this.keyLength}`);
+    this.key = typeof key !== 'undefined' ? key : this.generateRandomKey();
     for (let i = 0; i < this.key.length; i += 1) {
-      // console.log(!!!this.key)
-      // console.log(this.key.length)
-      // console.log(this.shiftString.charCodeAt(this.key.charAt(i)));
-      // console.log(this.key.charAt(i));
-      // console.log(this.key.charCodeAt(i));
-      // console.log(this.shiftString.charCodeAt(this.key.charAt(i)));
-      // console.log(Number.isNaN(this.shiftString.charCodeAt(this.key.charAt(i))));
-      // let isItNaN = Number.isNaN(this.shiftString.charCodeAt(this.key.charAt(i)));
-      // console.log(`is it a NaN? ${isItNaN}`);
-      // if (!this.shiftString.includes(this.key.charAt(i))) {
       const lowerCaseAUniCode = 97;
       const lowerCaseZUniCode = 122;
       if (
@@ -48,17 +24,10 @@ export class Cipher {
   }
 
   generateRandomKey(shiftString = this.shiftString, keyLength = this.keyLength) {
-    // console.log('generate random key!!!!!!!');
     let outputString = '';
-    // console.log(`keyLength is ${keyLength}`);
-    // console.log(`shiftString is ${shiftString}`);
-    // const charSet = shiftString;
     for (let i = 0; i < keyLength; i += 1) {
-      // console.log(`i is ${i}`);
       outputString += shiftString.charAt(Math.floor(Math.random() * shiftString.length));
-      // console.log(`character to enter is ${shiftString.charAt(Math.floor(Math.random() * shiftString.length))}`);
     }
-    // console.log(`Random key is: ${outputString}`);
     return outputString;
   }
 
@@ -95,10 +64,6 @@ export class Cipher {
       let keyNumber = this.shiftString.indexOf(encodedKey.charAt(i))
       let inputVsKey = inputNumber - keyNumber;
       let outputNumber = (inputVsKey + this.shiftString.length) % this.shiftString.length
-      // console.log(outputNumber);
-      // if (outputNumber < 0) {
-      //   outputNumber = this.shiftString.length + outputNumber
-      // }
       let outputLetter = this.shiftString.charAt(outputNumber)
       decodeOutputArray.push(outputLetter)
     }
@@ -106,8 +71,3 @@ export class Cipher {
     return decodeOutputString
   }
 }
-
-/*
-Had to really break my steps down. I was trying to do way too much on each line, so I
-couldn't really debug. After reviewing the cipher, I was able to get the code working.
-*/
